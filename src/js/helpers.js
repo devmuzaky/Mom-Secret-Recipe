71748d796c49b4ff26b71de75a634d552ac33e06
Mohamed Zaky <devmuzaky@gmail.com>
2022-10-28 23:43:15 +0200

    Create Helpers and Configuration Files

---
+++ src/js/helpers.js
@@ -0,0 +1,21 @@
+import {TIMEOUT_SEC} from "./config";
+
+const timeout = function (s) {
+    return new Promise(function (_, reject) {
+        setTimeout(function () {
+            reject(new Error(`Request took too long! Timeout after ${s} second`));
+        }, s * 1000);
+    });
+};
+
+export const getJSON = async function (url) {
+    try {
+        const fetchPro = fetch(url);
+        const res = await Promise.race([fetchPro, timeout(TIMEOUT_SEC)]);
+        const data = await res.json();
+            if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+            return data;
+    } catch (err) {
+        throw err;
+    }
+}
