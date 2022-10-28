daa5a21a009cf4fecb4748bf20325855c0d57634
Mohamed Zaky <devmuzaky@gmail.com>
2022-10-28 23:42:57 +0200

    work with MVC Architecture Refactoring for MVC

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,13 +1,32 @@
-const recipeContainer = document.querySelector('.recipe');
-
-const timeout = function (s) {
-  return new Promise(function (_, reject) {
-    setTimeout(function () {
-      reject(new Error(`Request took too long! Timeout after ${s} second`));
-    }, s * 1000);
-  });
-};
+import * as model from './model.js';
+import recipeView from './views/recipeView.js';
 
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
+
+
+const controlRecipes = async function () {
+    try {
+        const id = window.location.hash.slice(1);
+
+        if (!id) return;
+
+        recipeView.renderSpinner();
+
+        // 1) Loading recipe
+        await model.loadRecipe(id);
+
+        // Rendering recipe
+
+        recipeView.render(model.state.recipe);
+
+    } catch (err) {
+        alert(err);
+    }
+
+}
+
+// ['hashchange', 'load'].forEach(ev => window.addEventListener(ev, controlRecipes));
+window.addEventListener('hashchange', controlRecipes);
+window.addEventListener('load', controlRecipes);
---
+++ src/js/model.js
@@ -0,0 +1,39 @@
+import {async} from 'regenerator-runtime';
+import {API_URL} from './config.js';
+import {getJSON} from "./helpers";
+export const state = {
+    recipe: {},
+    search: {
+        query: '',
+        results: [],
+        page: 1,
+        resultsPerPage: 10,
+
+    },
+    bookmarks: [],
+
+
+}
+
+export const loadRecipe = async function (id) {
+    try {
+        const data = await getJSON(`${API_URL}/${id}`);
+
+        const {recipe} = data.data;
+        state.recipe = {
+            id: recipe.id,
+            title: recipe.title,
+            publisher: recipe.publisher,
+            sourceUrl: recipe.source_url,
+            image: recipe.image_url,
+            servings: recipe.servings,
+            cookingTime: recipe.cooking_time,
+            ingredients: recipe.ingredients
+        }
+        console.log(state.recipe)
+    } catch (err) {
+        alert(err)
+    }
+
+}
+
