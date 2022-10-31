b49f8e2215d138faa0b6629083b18211cc96ded6
Mohamed Zaky <devmuzaky@gmail.com>
2022-10-31 11:36:44 +0200

    update logo

--- src/js/views/view.js
+++ src/js/views/view.js
@@ -3,18 +3,36 @@ import icons from 'url:../../img/icons.svg';
 export default class View {
     _data;
 
+    /**
+     * Render the received object to the DOM
+     * @param {Object | Object[]} data The data to be rendered (e.g. recipe)
+     * @param {boolean} [render = true] true to render the result to the DOM, false to return the markup string
+     * @returns {undefined | string} A markup string is returned if render = false
+     * @this {Object} View instance (e.g. recipeView)
+     * @returns {void|*} A markup string is returned if render = false
+     * @author <a href="mail:devmuzaky@gmail.com">Mohamed Zaky</a>
+     */
     render(data, render = true) {
 
         if (!data || (Array.isArray(data) && data.length === 0)) return this.renderError();
         this._data = data;
         const markup = this._generateMarkup();
 
-        if(!render) return markup;
+        if (!render) return markup;
 
         this._clear();
         this._parentElement.insertAdjacentHTML('afterbegin', markup);
     }
 
+
+    /**
+     *
+     * @param {Object} data The data to be updated (e.g. recipe)
+     * @returns {void}  The updated markup is rendered to the DOM
+     * @description This function is used to update the DOM with new data without re-rendering the entire DOM
+     * @example this._update(data); // where this is the parent class
+
+     */
     update(data) {
         this._data = data;
         const newMarkup = this._generateMarkup();
